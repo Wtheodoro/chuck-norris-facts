fb56b38768ef190898833f7bbf24048a6cec4c9f
Walison Theodoro <theodoro.walison@gmail.com>
2022-06-20 15:53:38 -0300

    fix: tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
-export const content = [
-  './pages/**/*.{js,ts,jsx,tsx}',
-  './components/**/*.{js,ts,jsx,tsx}',
-]
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+}
